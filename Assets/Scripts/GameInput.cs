e58a83ba03f35f01d52470bd4d984191bdcc8783
Retmmmy <margaret805mp@gmail.com>
2025-07-04 02:19:47 +0300

    download please

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameInput : MonoBehaviour
+{
+    public static GameInput Instance { get; private set; }
+
+    private PlayerInputAction playerInputAction;
+    private void Awake()
+    {
+        Instance = this;
+        playerInputAction = new PlayerInputAction();
+        playerInputAction.Enable();
+    }
+
+    public Vector2 GetMovementVector()
+    {
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
+
+        return inputVector;
+    }
+
+   public Vector3 GetMousePosition() {
+       Vector3 mousePos =   Mouse.current.position.ReadValue();
+       return mousePos;
+   }
+}
+
+
